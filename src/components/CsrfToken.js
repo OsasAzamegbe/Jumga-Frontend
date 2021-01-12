c879bcbb3524c221992e240540bc3d06b0481bfc
osamudiamen <osas.azamegbe@gmail.com>
2021-01-12 19:05:30 +0100

    BUG: Removed "end" value on nav-menu justify

--- src/components/CsrfToken.js
+++ src/components/CsrfToken.js
@@ -5,12 +5,18 @@ import axios from 'axios';
 const CsrfToken = () => {
     useEffect(() =>{
         const loadCsrf = async () =>{
-            const url = `${process.env.REACT_APP_BACKEND_API_URL}/getcsrf`
-            await axios.get(url)
+            const url = `${process.env.REACT_APP_BACKEND_API_URL}/getcsrf`;
+            const config = {
+                "Accept": "application/json",
+                "Content-type": "application/json"
+            }
+            console.log(url)
+            const response = await axios.get(url, config);
+            console.log(response)
         };
-        
-        loadCsrf()
-    },[]);
+
+        loadCsrf();
+    }, []);
 
     return (
         <input hidden disabled/>
