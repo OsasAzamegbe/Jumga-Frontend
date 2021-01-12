d0e7f3b6108abc6623cb72c3431f583af82dd609
osamudiamen <osas.azamegbe@gmail.com>
2021-01-12 18:01:02 +0100

    FEAT: Mouted Router and Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
 
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+import Navbar from './components/Navbar';
+import Home from './pages/home/Home';
+
 const App = () => {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route exact to="/" component={Home}/>
+        </Switch>
+      </Router>
     </div>
   );
 };
